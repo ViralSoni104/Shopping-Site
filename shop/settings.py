05050f0ef63ce097a2c7cd307e18ad8341932b84
Viral Soni <sviral572@gmail.com>
2025-02-22 15:36:06 +0530

    updated allowed host

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = ["shopping-site-0vdu.onrender.com","localhost","127.0.0.1"]
 
 
 # Application definition
